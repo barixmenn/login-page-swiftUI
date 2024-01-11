fcf3c7579b213608541694ddb73d2c48fe2e93c1
baris <bdilekci200@gmail.com>
2024-01-11 16:47:14 +0300

    Initial Commit

---
+++ login-page/login_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_pageApp.swift
+//  login-page
+//
+//  Created by Baris Dilekci on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct login_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
