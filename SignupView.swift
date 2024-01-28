2619ac7e3a1a280e3f14014f3848b2f35dfaa606
baris <bdilekci200@gmail.com>
2024-01-29 00:41:09 +0300

    feat: create NavigationState

---
+++ SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  login-page
+//
+//  Created by Baris Dilekci on 29.01.2024.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
---
+++ login-page/Helper/NavigationState.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationState.swift
+//  login-page
+//
+//  Created by Baris Dilekci on 29.01.2024.
+//
+
+import SwiftUI
+
+struct NavigationState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationState()
+}
